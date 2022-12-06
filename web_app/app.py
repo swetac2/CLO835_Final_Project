24bcacb93e1f112f2532326c10e5f81543c29c8d
Ganishbansal <33124535+Ganishbansal@users.noreply.github.com>
2022-12-06 13:17:38 -0500

    Update app.py
    replacing color code with bg

--- web_app/app.py
+++ web_app/app.py
@@ -20,7 +20,7 @@ DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
 DBPORT = int(os.environ.get("DBPORT"))
 
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
+background_ENV = os.environ.get('APP_bg') or "set1"
 # Create a connection to the MySQL database
 db_conn = connections.Connection(
     host= DBHOST,
@@ -34,31 +34,28 @@ output = {}
 table = 'employee';
 
 # Define the supported color codes
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#89CFF0",
-    "blue2": "#30336b",
-    "pink": "#f4c2c2",
-    "darkblue": "#130f40",
-    "lime": "#C1FF9C",
+bg_codes = {
+    "set1": "",
+    "set2": "",
+    "set3": "",
+    "set4": "",
+
 }
 
-
 # Create a string of supported colors
-SUPPORTED_COLORS = ",".join(color_codes.keys())
+SUPPORTED_BG = ",".join(bg_codes.keys())
 
 # Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
+bg = random.choice(["set1", "set2", "set3", "set4"])
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
+    return render_template('addemp.html', bg=bg_codes[bg])
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR])
+    return render_template('about.html', bg=bg_codes[bg])
     
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -82,11 +79,11 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
+    return render_template('addempoutput.html', name=emp_name, bg=bg_codes[bg])
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    return render_template("getemp.html", bg=bg_codes[bg])
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -115,29 +112,29 @@ def FetchData():
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], bg=bg_codes[bg])
 
 if __name__ == '__main__':
     
-    # Check for Command Line Parameters for color
+    # Check for Command Line Parameters for bg
     parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
+    parser.add_argument('--bg', required=False)
     args = parser.parse_args()
 
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
+    if args.bg:
+        print("bg from command line argument =" + args.bg)
+        bg = args.bg
+        if background_ENV:
+            print("A bg was set through environment variable -" + background_ENV + ". However, bg from command line argument takes precendence.")
+    elif background_ENV:
+        print("No Command line argument. bg from environment variable =" + background_ENV)
+        bg = background_ENV
     else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
+        print("No command line argument or environment variable. Picking a Random bg =" + bg)
 
     # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
+    if bg not in bg_codes:
+        print("bg not supported. Received '" + bg + "' expected one of " + background_COLORS)
         exit(1)
 
-    app.run(host='0.0.0.0',port=8080,debug=True)
+    app.run(host='0.0.0.0',port=81,debug=True)
